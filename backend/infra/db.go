5550e70f0617990b3a43433c66c61a2bb89e72a5
ccbc7 <flexibleelbow@gmail.com>
2025-02-26 00:00:34 +0900

    🚑 db接続設定修正

--- backend/infra/db.go
+++ backend/infra/db.go
@@ -14,11 +14,11 @@ func SetupDB() *gorm.DB {
 	env := os.Getenv("ENV")
 	dsn := fmt.Sprintf(
 		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Tokyo",
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"),
-		os.Getenv("DB_PORT"),
+		os.Getenv("POSTGRES_HOST"),
+		os.Getenv("POSTGRES_USER"),
+		os.Getenv("POSTGRES_PASSWORD"),
+		os.Getenv("POSTGRES_DB"),
+		os.Getenv("POSTGRES_PORT"),
 	)
 
 	var (
@@ -26,7 +26,7 @@ func SetupDB() *gorm.DB {
 		err error
 	)
 
-	if env == "prod" {
+	if env == "dev" {
 		// dnsを指定してDBに接続
 		db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 		log.Println("Setup postgrresql database")
