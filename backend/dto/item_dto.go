9e9c29f6a3af0228217f7e035d051f1eff2ff1c9
ccbc7 <flexibleelbow@gmail.com>
2025-02-28 00:10:03 +0900

    🎨 example文追加

--- backend/dto/item_dto.go
+++ backend/dto/item_dto.go
@@ -3,8 +3,8 @@ package dto
 type CreateItemInput struct {
 	// json:"name"はJSONのキー名を指定し、間違っている場合はエラーを返す
 	Name        string `json:"name" binding:"required,min=2"`
-	Price       uint   `json:"price" binding:"required,min=1,max=999999"`
-	Description string `json:"description"`
+	Price       uint   `json:"price" binding:"required,min=1,max=999999" example:"100"`
+	Description string `json:"description" example:"This is item1"`
 }
 
 type UpdateItemInput struct {
