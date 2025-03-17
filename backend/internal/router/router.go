0f8efa6f15a8158c72022b51bad241aed73a16fc
ccbc7 <flexibleelbow@gmail.com>
2025-03-17 22:18:25 +0900

    ♻️ ルーティングの内容を別ディレクトリに設置

---
+++ backend/internal/router/router.go
@@ -0,0 +1,71 @@
+package router
+
+import (
+	controllers "project/internal/handlers"
+	"project/internal/middlewares"
+	"project/internal/repositories"
+	"project/internal/services"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	"gorm.io/gorm"
+
+	"project/docs"
+)
+
+func SetupRouter(db *gorm.DB) *gin.Engine {
+	itemRepository := repositories.NewItemRepository(db)
+	itemService := services.NewItemService(itemRepository)
+	itemController := controllers.NewItemController(itemService)
+
+	authRepository := repositories.NewAuthRepository(db)
+	authService := services.NewAuthService(authRepository)
+	authController := controllers.NewAuthController(authService)
+
+	csvRepository := repositories.NewCsvRepository(db)
+	filepath := "./data/sample_data_100000.csv"
+	csvService := services.NewCsvService(csvRepository, filepath)
+	csvController := controllers.NewCsvController(csvService)
+
+	// ルーターの作成
+	r := gin.Default()
+
+	r.Use(cors.Default())
+
+	// Swaggerの設定
+	docs.SwaggerInfo.BasePath = "/api/v1"
+	v1 := r.Group("/api/v1")
+	{
+		eg := v1.Group("/")
+		{
+			eg.GET("/hello", controllers.HelloWorld)
+		}
+
+		// ルーティンググループの作成
+		itemRouter := v1.Group("/items")
+		itemRouterWithAuth := v1.Group("/items", middlewares.AuthMiddleware(authService))
+		authRouter := v1.Group("/auth")
+		csvRouter := v1.Group("/csv")
+
+		// ルーティングの設定
+		itemRouter.GET("", itemController.FindAll)
+
+		itemRouterWithAuth.GET("/:id", itemController.FindById)
+		itemRouterWithAuth.POST("", itemController.Create)
+		itemRouterWithAuth.PUT("/:id", itemController.Update)
+		itemRouterWithAuth.DELETE("/:id", itemController.Delete)
+
+		authRouter.POST("/signup", authController.SignUp)
+		authRouter.POST("/login", authController.Login)
+
+		csvRouter.POST("/process", csvController.ProcessCsv)
+	}
+
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
+
+	r.GET("/", controllers.HelloWorld)
+
+	return r
+}
